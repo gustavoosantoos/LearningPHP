5048f7dad8393e22675f75a8485d0f5a566f1d10
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-06-10 14:58:29 -0300

    formulário com estilos

--- forms.php
+++ forms.php
@@ -2,19 +2,34 @@
 <head>
     <meta charset="UTF-8">
     <title>Primeiro exemplo PHP</title>
+
+    <?php
+    $texto = isset($_GET['text']) ? $_GET["text"] : "Não informado";
+    $tamanho = isset($_GET['tamanho']) ? $_GET["tamanho"] : 12;
+    $cor = isset($_GET['cor']) ? $_GET["cor"] : "#000000";
+    ?>
+
+    <style>
+        p {
+            color: <?= $cor ?>;
+            font-size: <?= $tamanho ?>pt;
+        }
+    </style>
 </head>
 <body>
 <div>
     <h1>Aprendendo PHP - Atribuções</h1>
-    <?php
-    $valor = isset($_GET['valor']) ? $_GET["valor"] : "Não informado";
-    echo "$valor";
-    ?>
 
     <form method="get" action="forms.php">
-        Valor: <input type="number" name="valor">
+        Text: <input type="text" name="text">
+        Cor: <input type="color" name="cor">
+        Tamanho: <input type="number" name="tamanho">
         <input type="submit" value="Calcular">
     </form>
+
+    <p>
+        <?= $texto ?>
+    </p>
 </div>
 </body>
 </html>
